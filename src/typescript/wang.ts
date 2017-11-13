24f32ae8d2372af23c9e4a1bc10597becaccb175
John Haugeland <stonecypher@gmail.com>
2017-11-12 19:41:48 -0800

    touch

--- src/typescript/wang.ts
+++ src/typescript/wang.ts
@@ -1,86 +1,87 @@
 
 %% @since Version 835
 
-ms_wang() ->
+const ms_wang = () =>
 
-    [ {r,y,g,b}, {g,b,g,b}, {r,y,r,y}, {g,b,r,y}, {r,b,g,y}, {g,y,g,y}, {r,b,r,b}, {g,y,r,b} ].
-
-
-
-
-
-%% @since Version 835
-
-wang_row(Width) ->
-
-    wang_row(Width, top_row, 1, '_', []).
+    [ {r,y,g,b},
+      {g,b,g,b},
+      {r,y,r,y},
+      {g,b,r,y},
+      {r,b,g,y},
+      {g,y,g,y},
+      {r,b,r,b},
+      {g,y,r,b} ];
 
 
 
 
 
-%% @since Version 835
+const wang_row = (Width) =>
 
-wang_row(Width, PriorRow) ->
-
-    wang_row(Width, PriorRow, 1, '_', []).
+    wang_row(Width, top_row, 1, '_', []);
 
 
 
 
 
-%% @since Version 835
+const wang_row = (Width, PriorRow) =>
 
-wang_row(0, top_row, _At, _Prev, Work) ->
-
-    list_to_tuple(lists:reverse(Work));
+    wang_row(Width, PriorRow, 1, '_', []);
 
 
 
 
 
-wang_row(Remaining, top_row, At, Prev, Work) ->
+const wang_row = (0, top_row, _At, _Prev, Work) =>
 
-    N = wang_such_that(case Prev of '_' -> '_'; {_,R,_,_} -> R end, '_'),
-
-    wang_row(Remaining-1, top_row, At+1, N, [N] ++ Work);
+    list_to_tuple( Work.reverse() );
 
 
 
 
 
-wang_row(0, _OldTopRow, _At, _Prev, Work) ->
+const wang_row = (Remaining, top_row, At, Prev, Work) => {
 
-    list_to_tuple(lists:reverse(Work));
+    const N = wang_such_that(case Prev of '_' -> '_'; {_,R,_,_} -> R end, '_');
 
+    return wang_row(Remaining-1, top_row, At+1, N, [N] ++ Work);
 
-
-
-
-wang_row(Remaining, OldTopRow, At, Prev, Work) ->
-
-    T = element(At, OldTopRow),
-    N = wang_such_that(case Prev of '_' -> '_'; {_,R,_,_} -> R end, case T of '_' -> '_'; {_,_,B,_} -> B end),
-
-    wang_row(Remaining-1, OldTopRow, At+1, N, [N] ++ Work).
+}
 
 
 
 
 
-%% @since Version 835
+const wang_row = (0, _OldTopRow, _At, _Prev, Work) =>
 
-wang_such_that(Left, Top) ->
-
-    wang_such_that(Left, Top, ms_wang()).
+    list_to_tuple( Work.reverse() );  // todo
 
 
 
 
 
-%% @since Version 835
+const wang_row = (Remaining, OldTopRow, At, Prev, Work) => {
 
-wang_such_that('_', '_', Tiles) ->
+    const T = element(At, OldTopRow),
+          N = wang_such_that(case Prev of '_' -> '_'; {_,R,_,_} -> R end, case T of '_' -> '_'; {_,_,B,_} -> B end);
+
+    return wang_row(Remaining-1, OldTopRow, At+1, N, [N] ++ Work);
+
+}
+
+
+
+
+
+const wang_such_that = (Left, Top) =>
+
+    wang_such_that(Left, Top, ms_wang());
+
+
+
+
+
+const wang_such_that = ('_', '_', Tiles) =>
 
     sc:random_from(Tiles);
 
@@ -88,7 +89,7 @@ wang_such_that('_', '_', Tiles) ->
 
 
 
-wang_such_that('_', Top, Tiles) ->
+const wang_such_that = ('_', Top, Tiles) =>
 
     sc:random_from([ Satisfied || { T,_R,_B,_L}=Satisfied <- Tiles, T =:= Top  ]);
 
@@ -96,39 +97,35 @@ wang_such_that('_', Top, Tiles) ->
 
 
 
-wang_such_that(Left, '_', Tiles) ->
+const wang_such_that = (Left, '_', Tiles) =>
 
-    sc:random_from([ Satisfied || {_T,_R,_B, L}=Satisfied <- Tiles, L =:= Left  ]);
-
-
-
-
-
-wang_such_that(Left, Top, Tiles) ->
-
-    sc:random_from([ Satisfied || { T,_R,_B, L}=Satisfied <- Tiles, T =:= Top, L =:= Left  ]).
+    sc:random_from([ Satisfied || {_T,_R,_B, L}=Satisfied <- Tiles, L =:= Left  ]);  // todo
 
 
 
 
 
-%% @since Version 835
+const wang_such_that = (Left, Top, Tiles) =>
 
--spec wang_carpet( integer(), integer() ) -> { { integer() } }.
-
-wang_carpet(X, Y) ->
-
-    wang_carpet(X,Y, []).
+    sc:random_from([ Satisfied || { T,_R,_B, L}=Satisfied <- Tiles, T =:= Top, L =:= Left  ]).  // lol todo
 
 
 
 
 
-%% @since Version 835
+// -spec wang_carpet( integer(), integer() ) -> { { integer() } }.
 
-wang_carpet(X, Y, Tiles) ->
+const wang_carpet = (X, Y) =>
 
-    wang_carpet(X, Y, Tiles, top_row, []).
+    wang_carpet(X,Y, []);
+
+
+
+
+
+const wang_carpet = (X, Y, Tiles) =>
+
+    wang_carpet(X, Y, Tiles, top_row, []);
 
 
 
